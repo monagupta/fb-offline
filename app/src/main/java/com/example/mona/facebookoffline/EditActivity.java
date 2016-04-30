90495e40c4e50c58992ba84a81171eb44b93ed0e
Wes Hunt <wesleyahunt@gmail.com>
2016-04-30 16:42:08 -0700

    Remove unused import statments

--- app/src/main/java/com/example/mona/facebookoffline/EditActivity.java
+++ app/src/main/java/com/example/mona/facebookoffline/EditActivity.java
@@ -1,13 +1,7 @@
 package com.example.mona.facebookoffline;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
