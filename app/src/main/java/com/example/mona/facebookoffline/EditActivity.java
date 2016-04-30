09046507f3204729cbacbd7d5f61ebcdb51e47b3
Wes Hunt <wesleyahunt@gmail.com>
2016-04-30 16:21:31 -0700

    Add edit post screen for creating and editing posts
    You can view it by pressing the create post button. The post button does nothing. The save button goes back. It does not save your text

---
+++ app/src/main/java/com/example/mona/facebookoffline/EditActivity.java
@@ -0,0 +1,33 @@
+package com.example.mona.facebookoffline;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class EditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        Button button = (Button) findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mona/facebookoffline/MainActivity.java
+++ app/src/main/java/com/example/mona/facebookoffline/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mona.facebookoffline;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.create_post);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
     }
 }
 
